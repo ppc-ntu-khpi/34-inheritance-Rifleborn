49bd919721464a7c2a5d0643e3c0a8aed22858b6
Rifleborn <49074281+Rifleborn@users.noreply.github.com>
2021-02-02 16:48:56 +0200

    Add files via upload

--- src/test/TestAnimal.java
+++ src/test/TestAnimal.java
@@ -1,15 +1,17 @@
 package test;
-
-import domain.Cat;
+import domain.Psy_Dog;
 
 public class TestAnimal {
 
     public static void main(String[] args) {
-        Cat cat = new Cat("Thomas");
-        System.out.println(cat);
-        cat.hunt();
-        cat.eat();
-        cat.speak();
-        cat.play();
+        Psy_Dog psy_dog = new Psy_Dog("Nipper");
+        
+        System.out.println(psy_dog);
+        
+        psy_dog.toString();
+        psy_dog.speak();
+        psy_dog.Attack();
+        psy_dog.Create_Phantom(6);
+        psy_dog.eat();
     }
 }
